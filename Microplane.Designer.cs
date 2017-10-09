3175a2e4d68f95b02bbb10fbefd6f245e8c863d4
valhag <valhag@terra.com.mx>
2017-10-09 17:44:58 -0500

    formmicroplane

---
+++ Microplane.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace InterfazAdmin
+{
+    partial class Microplane
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.empresasComercial1 = new Controles.EmpresasComercial();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBD = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 67);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 17);
+            this.label1.TabIndex = 44;
+            this.label1.Text = "Concepto";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(140, 63);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(346, 21);
+            this.comboBox1.TabIndex = 43;
+            // 
+            // empresasComercial1
+            // 
+            this.empresasComercial1.Location = new System.Drawing.Point(16, 11);
+            this.empresasComercial1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.empresasComercial1.Name = "empresasComercial1";
+            this.empresasComercial1.Size = new System.Drawing.Size(650, 54);
+            this.empresasComercial1.TabIndex = 42;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(27, 117);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(111, 16);
+            this.label11.TabIndex = 47;
+            this.label11.Text = "Contraseña Sello";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(140, 115);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.PasswordChar = '*';
+            this.textBox3.Size = new System.Drawing.Size(76, 20);
+            this.textBox3.TabIndex = 46;
+            this.textBox3.Text = "12345678a";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 358);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(569, 23);
+            this.button1.TabIndex = 45;
+            this.button1.Text = "Enviar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(17, 287);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(243, 23);
+            this.button2.TabIndex = 56;
+            this.button2.Text = "Test y Guardar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(17, 252);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 20);
+            this.label4.TabIndex = 55;
+            this.label4.Text = "Password";
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(122, 252);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(138, 20);
+            this.txtPass.TabIndex = 54;
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(17, 226);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 20);
+            this.label3.TabIndex = 53;
+            this.label3.Text = "Usuario";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(122, 226);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(138, 20);
+            this.txtUser.TabIndex = 52;
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(17, 198);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 20);
+            this.label2.TabIndex = 51;
+            this.label2.Text = "Base Datos";
+            // 
+            // txtBD
+            // 
+            this.txtBD.Location = new System.Drawing.Point(122, 198);
+            this.txtBD.Name = "txtBD";
+            this.txtBD.Size = new System.Drawing.Size(138, 20);
+            this.txtBD.TabIndex = 50;
+            this.txtBD.Text = "GeneralesSQL";
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(16, 172);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 20);
+            this.label5.TabIndex = 49;
+            this.label5.Text = "Server";
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(122, 172);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(138, 20);
+            this.txtServer.TabIndex = 48;
+            // 
+            // Microplane
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 395);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBD);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtServer);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.empresasComercial1);
+            this.Name = "Microplane";
+            this.Text = "Microplane";
+            this.Load += new System.EventHandler(this.Microplane_Load);
+            this.Shown += new System.EventHandler(this.Microplane_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private Controles.EmpresasComercial empresasComercial1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBD;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtServer;
+
+    }
+}
---
+++ Microplane.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using LibreriaDoctos;
+using System.IO;
+
+namespace InterfazAdmin
+{
+    public partial class Microplane : Form
+    {
+
+        ClassRN lrn = new ClassRN();
+        public string Cadenaconexion = "";
+
+        public Microplane()
+        {
+            InitializeComponent();
+                       Properties.Settings.Default.database = "CompacWAdmin";
+            Properties.Settings.Default.Save();
+            Cadenaconexion = "data source =" + Properties.Settings.Default.server +
+      ";initial catalog =" + Properties.Settings.Default.database + " ;user id = " + Properties.Settings.Default.user +
+      "; password = " + Properties.Settings.Default.password + ";";
+        }
+
+        private void Microplane_Load(object sender, EventArgs e)
+        {
+            this.Text = " Interfaz Microplane " + " " + this.ProductVersion;
+
+            txtServer.Text = Properties.Settings.Default.serverOrigen;
+            txtBD.Text = Properties.Settings.Default.databaseOrigen;
+            txtUser.Text = Properties.Settings.Default.userOrigen;
+            txtPass.Text = Properties.Settings.Default.passwordO;
+
+
+            lrn.mSeteaDirectorio(Directory.GetCurrentDirectory());
+
+            if (Cadenaconexion != "" && Cadenaconexion != "data source =;initial catalog =CompacWAdmin ;user id = ; password = ;")
+            {
+                empresasComercial1.Populate(Cadenaconexion);
+
+
+                mCargaConceptos();
+                empresasComercial1.SelectedItem += new EventHandler(OnComboChange);
+            }
+            else
+            {
+                Form5 x = new Form5();
+                x.ShowDialog(this);
+            }
+        }
+
+        private void Microplane_Shown(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void OnComboChange(object sender, EventArgs e)
+        {
+            mCargaConceptos();
+
+        }
+
+
+
+
+        private void mCargaConceptos()
+        {
+            Properties.Settings.Default.RutaEmpresaADM = empresasComercial1.aliasbdd;
+            Properties.Settings.Default.Save();
+
+            List<RegConcepto> _RegFacturas = new List<RegConcepto>();
+            _RegFacturas = lrn.mCargarConceptosFacturacfdiComercial();
+
+            if (_RegFacturas.Count > 0)
+            {
+                comboBox1.DataSource = null;
+                comboBox1.Items.Clear();
+                comboBox1.DataSource = _RegFacturas;
+                comboBox1.DisplayMember = "Nombre";
+                comboBox1.ValueMember = "Codigo";
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+
+            Properties.Settings.Default.Pass = textBox3.Text;
+            Properties.Settings.Default.RutaEmpresaADM = empresasComercial1.aliasbdd;
+            RegConcepto Factura = (RegConcepto)comboBox1.SelectedItem;
+            Properties.Settings.Default.Concepto = Factura.Codigo.Trim();
+
+
+
+            Properties.Settings.Default.serverOrigen = txtServer.Text;
+            Properties.Settings.Default.databaseOrigen = txtBD.Text;
+            Properties.Settings.Default.userOrigen = txtUser.Text;
+            Properties.Settings.Default.passwordO = txtPass.Text;
+
+
+            Properties.Settings.Default.Save();
+
+            //string archivo = textBox1.Text;
+            lrn.mLlenarinfoMicroplane();
+
+            List<string> lista = new List<string>();
+
+            bool incluyetimbrado = true;
+            //lista = lrn.mGrabarDoctosComercial(1);
+            if (lista.Count != 0)
+            {
+                MessageBox.Show(lista[0].ToString());
+            }
+            else
+                MessageBox.Show("Proceso Terminado");
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string Cadenaconexion = "data source =" + txtServer.Text + ";initial catalog =" + txtBD.Text + ";user id = " + txtUser.Text + "; password = " + txtPass.Text + ";";
+            System.Data.SqlClient.SqlConnection _con = new System.Data.SqlClient.SqlConnection();
+
+            _con.ConnectionString = Cadenaconexion;
+            try
+            {
+                _con.Open();
+                // si se conecto grabar los datos en el cnf
+                _con.Close();
+                MessageBox.Show("Conexion Correcta");
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show("Revise datos de conexion");
+            }
+        }
+    }
+}
