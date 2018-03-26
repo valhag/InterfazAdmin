4290c95ed9a456ebe11a512feab318bc9e6091e9
valhag <hectorvalagui@hotmail.com>
2018-03-25 20:04:53 -0600

    PedidosFacturas

---
+++ PedidosFacturas.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace InterfazAdmin
+{
+    partial class PedidosFacturas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.botonExcel1 = new controles.BotonExcel();
+            this.empresasComercial1 = new Controles.EmpresasComercial();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(594, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // botonExcel1
+            // 
+            this.botonExcel1.Location = new System.Drawing.Point(20, 11);
+            this.botonExcel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.botonExcel1.Name = "botonExcel1";
+            this.botonExcel1.Size = new System.Drawing.Size(533, 29);
+            this.botonExcel1.TabIndex = 41;
+            // 
+            // empresasComercial1
+            // 
+            this.empresasComercial1.Location = new System.Drawing.Point(20, 57);
+            this.empresasComercial1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.empresasComercial1.Name = "empresasComercial1";
+            this.empresasComercial1.Size = new System.Drawing.Size(609, 54);
+            this.empresasComercial1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 113);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 17);
+            this.label1.TabIndex = 43;
+            this.label1.Text = "Facturas";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(146, 110);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(346, 21);
+            this.comboBox1.TabIndex = 42;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 144);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 17);
+            this.label2.TabIndex = 45;
+            this.label2.Text = "Pedidos";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(146, 141);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(346, 21);
+            this.comboBox2.TabIndex = 44;
+            // 
+            // PedidosFacturas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 245);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.botonExcel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.empresasComercial1);
+            this.Name = "PedidosFacturas";
+            this.Text = "PedidosFacturas";
+            this.Load += new System.EventHandler(this.PedidosFacturas_Load);
+            this.Shown += new System.EventHandler(this.PedidosFacturas_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Controles.EmpresasComercial empresasComercial1;
+        private System.Windows.Forms.Button button1;
+        private controles.BotonExcel botonExcel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
---
+++ PedidosFacturas.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using LibreriaDoctos;
+using Interfaces;
+using System.IO;
+
+namespace InterfazAdmin
+{
+    public partial class PedidosFacturas : Form, IObservador
+    {
+        ClassRN lrn = new ClassRN();
+        public string Cadenaconexion = "";
+        List<string> listaerrores = new List<string>();
+     
+        public PedidosFacturas()
+        {
+            
+            InitializeComponent();
+            Properties.Settings.Default.database = "CompacWAdmin";
+            Properties.Settings.Default.Save();
+            Cadenaconexion = "data source =" + Properties.Settings.Default.server +
+      ";initial catalog =" + Properties.Settings.Default.database + " ;user id = " + Properties.Settings.Default.user +
+      "; password = " + Properties.Settings.Default.password + ";";
+        }
+
+        public void Actualizar(double message)
+        {
+            //int x = int(message);
+            try
+            {
+                //this.progressBar1.Value = Convert.ToInt32(message);
+            }
+            catch (Exception eeeeee)
+            { }
+        }
+
+        public void Actualizar(string message)
+        {
+            //int x = int(message);
+            listaerrores.Add(message);
+        }
+
+        private void PedidosFacturas_Load(object sender, EventArgs e)
+        {
+            if (this == null)
+            {
+               
+            }
+        }
+        private void OnComboChange(object sender, EventArgs e)
+        {
+            mCargaConceptos();
+
+        }
+
+
+        private void mCargaConceptos()
+        {
+            Properties.Settings.Default.RutaEmpresaADM = empresasComercial1.aliasbdd;
+            Properties.Settings.Default.Save();
+
+            List<RegConcepto> _RegFacturas = new List<RegConcepto>();
+            List<RegConcepto> _RegPedidos = new List<RegConcepto>();
+            
+            _RegFacturas = lrn.mCargarConceptosFacturacfdiComercial();
+
+
+            
+            if (_RegFacturas.Count > 0)
+            {
+                comboBox1.DataSource = null;
+                comboBox1.Items.Clear();
+                comboBox1.DataSource = _RegFacturas;
+                comboBox1.DisplayMember = "Nombre";
+                comboBox1.ValueMember = "Codigo";
+            }
+
+            _RegPedidos = lrn.mCargarConceptosPedidosComercial();
+            if (_RegPedidos.Count > 0)
+            {
+                comboBox2.DataSource = null;
+                comboBox2.Items.Clear();
+                comboBox2.DataSource = _RegPedidos;
+                comboBox2.DisplayMember = "Nombre";
+                comboBox2.ValueMember = "Codigo";
+            }
+            /*_RegPagos = lrn.mCargarConceptosPagocfdiComercial();
+            if (_RegPagos.Count > 0)
+            {
+                comboBox3.DataSource = null;
+                comboBox3.Items.Clear();
+                comboBox3.DataSource = _RegPagos;
+                comboBox3.DisplayMember = "Nombre";
+                comboBox3.ValueMember = "Codigo";
+            }
+            _RegNC = lrn.mCargarConceptosNCcfdiComercial();
+            if (_RegNC.Count > 0)
+            {
+                comboBox4.DataSource = null;
+                comboBox4.Items.Clear();
+                comboBox4.DataSource = _RegNC;
+                comboBox4.DisplayMember = "Nombre";
+                comboBox4.ValueMember = "Codigo";
+            }*/
+
+        }
+
+        private void PedidosFacturas_Shown(object sender, EventArgs e)
+        {
+            ISujeto lsujeto = lrn.lbd;
+            lsujeto.Registrar(this);
+
+            this.Text = " Interfaz Pedidos/facturas " + " " + this.ProductVersion;
+            lrn.mSeteaDirectorio(Directory.GetCurrentDirectory());
+
+            if (Cadenaconexion != "" && Cadenaconexion != "data source =;initial catalog =CompacWAdmin ;user id = ; password = ;")
+            {
+                empresasComercial1.Populate(Cadenaconexion);
+                mCargaConceptos();
+                empresasComercial1.SelectedItem += new EventHandler(OnComboChange);
+            }
+            else
+            {
+                this.Visible = false;
+                Form5 x = new Form5();
+                //this.Visible = false;
+                x.ShowDialog(this);
+            }
+            botonExcel1.mGeneraNombre(2, "CARGA DE PEDIDOS");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Properties.Settings.Default.Pass = textBox3.Text;
+            Properties.Settings.Default.RutaEmpresaADM = empresasComercial1.aliasbdd;
+            //RegConcepto Factura = (RegConcepto)comboBox1.SelectedItem;
+            //Properties.Settings.Default.Concepto = Factura.Codigo.Trim();
+            Properties.Settings.Default.Save();
+
+            string archivo = botonExcel1.mRegresarNombre();
+            lrn.mLLenarInfoPedidosFacturas(archivo);
+
+            List<string> lista = new List<string>();
+
+            bool incluyetimbrado = true;
+            lista = lrn.mGrabarDoctos(incluyetimbrado, 1);
+            if (lista.Count != 0)
+            {
+                MessageBox.Show(lista[0].ToString());
+            }
+            else
+                MessageBox.Show("Proceso Terminado");
+        }
+    }
+}
